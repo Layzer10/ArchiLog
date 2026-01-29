0ad2c47722c4115602c2dc4bb682b797e66a85c2
22507444 <esteban.cazor@gmail.com>
2026-01-29 17:53:25 +0100

    changement 1

--- src/archilog/__init__.py
+++ src/archilog/__init__.py
@@ -3,7 +3,8 @@ import uuid
 
 from dataclasses import dataclass
 
-
+import sqlite3
+from datetime import date
 @dataclass
 class Item:
     id: uuid.UUID
@@ -17,6 +18,28 @@ def cli():
 
 @cli.command()
 @click.option("-n", "--name", prompt="Name", help="The name of the item.")
-def display(name: str):
+def createCagnote(name: str):
     item = Item(uuid.uuid4(), name)
     click.echo(item)
+
+@cli.command()
+@click.option("-n", "--name", prompt="Name", help="The name of the item.")
+def deleteCagnote(id: int):
+    item = Item(uuid.uuid4(), int)
+    click.echo(item.name)
+
+@cli.command()
+@click.option('--item',type=(str, int, click.DateTime(formats=["%Y-%m-%d"])),
+required=True)
+def addParticipation(item):
+    name, prix, date = item
+
+    click.echo(f"nom={name} prix={prix} date={date.date()}")
+
+@cli.command()
+@click.option("-n", "--name", prompt="Name", help="The name of the item.")
+def deleteParticipation(id: int):
+    item = Item(uuid.uuid4(), id)
+    click.echo(item)
+
+createCagnote()
